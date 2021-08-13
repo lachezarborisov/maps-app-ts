24e1bc1b2186fc15ce5f5388c4e04c916fad35d9
Lachezar Borisov <ly.borisov@gmail.com>
2021-08-13 17:27:45 +0300

    Initial commit

---
+++ src/Users.ts
@@ -0,0 +1,25 @@
+import faker from 'faker';
+
+export class User {
+    name: string;
+    location: {
+        lat: number;
+        lng: number;
+    };
+
+    constructor() {
+        this.name = faker.name.firstName();
+        this.location = {
+            lat: parseFloat(faker.address.latitude()),
+            lng: parseFloat(faker.address.longitude())
+        };
+    }
+
+    markerContent(): string {
+        return `
+            <div>
+                <h2>User Name: ${this.name}</h2>  
+            </div>
+        `;
+    }
+}
